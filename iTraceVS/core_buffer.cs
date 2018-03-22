4687d5c60e5311a55999a3b73f28cef77e085af5
Corey <cbryan20@kent.edu>
2018-03-22 11:20:13 -0400

    begin buffered data implementation

---
+++ iTraceVS/core_buffer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace iTraceVS
+{
+    class core_buffer
+    {
+        Queue<core_data> buffer;
+        private static Mutex mutex = new Mutex();
+        private static core_buffer singleton;
+
+        private core_buffer() { }
+
+        public static core_buffer Instance {
+            get {
+                if (singleton == null) {
+                    singleton = new core_buffer();
+                }
+                return singleton;
+            }
+        }
+
+        core_data dequeue() {
+            core_data cd = new core_data();
+
+            while (buffer.Count == 0) {
+                Thread.Sleep(10);
+            }
+
+            if (mutex.WaitOne(500)) {
+                cd = buffer.Dequeue();
+                mutex.ReleaseMutex();
+            }
+
+            return cd;
+        }
+
+        void enqueue(core_data cd) {
+            if (mutex.WaitOne(500)) {
+                buffer.Enqueue(cd);
+                mutex.ReleaseMutex();
+            }
+        }
+        
+        ~core_buffer() {
+            mutex.Dispose();
+        }
+    }
+}
---
+++ iTraceVS/core_data.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTraceVS
+{
+    class core_data
+    {
+        double eyeX;
+        double eyeY;
+        Int64 sessionTime;
+
+        public core_data() {
+            eyeX = -1;
+            eyeY = -1;
+            sessionTime = -1;
+        }
+
+        public core_data(string data) {
+            int i = 0;
+            string x = "", y = "", session = "";
+
+            while (data[i] != ',') {
+                session += data[i];
+                ++i;
+            }
+            sessionTime = Convert.ToInt64(session);
+            ++i; //move past the ','
+            
+            while (data[i] != ',') {
+                x += data[i];
+                ++i;
+            }
+            ++i; //move past the ','
+
+            while (i < data.Length) {
+                y += data[i];
+                ++i;
+            }
+
+            if (x == "-nan(ind)" || y == "-nan(ind") {
+                eyeX = -1;
+                eyeY = -1;
+            } else {
+                eyeX = Convert.ToDouble(x);
+                eyeY = Convert.ToDouble(y);
+            }
+        }
+
+        ~core_data() { }      
+    }
+}
