6cc9da31b3226a80a74f102423850f01623fa2de
Drew Guarnera <dguarner@kent.edu>
2019-04-02 15:40:14 -0400

    Add support for new core input format
    Change output filename to match conventions.

--- iTraceVS/core_data.cs
+++ iTraceVS/core_data.cs
@@ -8,46 +8,52 @@ namespace iTraceVS
         public double eyeY;
         public Int64 sessionTime;
 
-        public core_data() {
+        public core_data()
+        {
             eyeX = -1;
             eyeY = -1;
             sessionTime = -1;
         }
 
-        public core_data(string data) {
+        public core_data(string data)
+        {
             string[] data_string;
 
             data_string = data.Split(',');
 
-            if (data_string[0] == "gaze") {
+            if (data_string[0] == "gaze")
+            {
                 sessionTime = Convert.ToInt64(data_string[1]);
 
-                if (data_string[2] == "nan" || data_string[3] == "nan") {
+                if (data_string[2] == "nan" || data_string[3] == "nan")
+                {
                     eyeX = -1;
                     eyeY = -1;
                 }
-                else {
+                else
+                {
                     eyeX = Convert.ToDouble(data_string[2]);
                     eyeY = Convert.ToDouble(data_string[3]);
                 }
             }
-            else if (data_string[0] == "session") {                
+            else if (data_string[0] == "session_start")
+            {
                 eyeX = -1;
                 eyeY = -1;
                 sessionTime = -1;
-
-                string[] tmp = data_string[1].Split('\\');
-                if (xml_writer.filePath == "default.xml") {
-                    xml_writer.filePath = data_string[1] + "/visualStudio_" + tmp[tmp.Length - 1] + ".xml";
+                System.Diagnostics.Debug.WriteLine(data);
+                if (xml_writer.filePath == "default.xml")
+                {
+                    xml_writer.filePath = data_string[3] + "/itrace_msvs_" + data_string[2] + ".xml";
                     xml_writer.xmlStart();
-                } else {
+                }
+                else
+                {
                     xml_writer.xmlEnd();
-                    xml_writer.filePath = data_string[1] + "/visualStudio_" + tmp[tmp.Length - 1] + ".xml";
+                    xml_writer.filePath = data_string[3] + "/itrace_msvs_" + data_string[2] + ".xml";
                     xml_writer.xmlStart();
                 }
             }
         }
-
-        //~core_data() { }      
     }
 }
