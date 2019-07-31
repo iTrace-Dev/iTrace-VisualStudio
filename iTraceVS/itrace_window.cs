a0bc9e7b4d67ae3b85ce8474f5400a0c004ea689
Drew Guarnera <dguarner@kent.edu>
2019-07-31 10:59:29 -0400

    Change plugin window title
    Was a previously generic identifier like title.
    Now called "iTraceVS Plugin" to better match the
    eclipse version.

--- iTraceVS/itrace_window.cs
+++ iTraceVS/itrace_window.cs
@@ -23,7 +23,7 @@
         /// </summary>
         public itrace_window() : base(null)
         {
-            this.Caption = "itrace_window";
+            this.Caption = "iTraceVS Plugin";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
