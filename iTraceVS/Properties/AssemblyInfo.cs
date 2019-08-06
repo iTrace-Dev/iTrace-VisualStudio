6b5ea73d6a79b64591345c30351a516ebd663661
Drew Guarnera <dguarner@kent.edu>
2019-08-06 18:23:39 -0400

    Change plugin name to "iTrace VS Plugin"

--- iTraceVS/Properties/AssemblyInfo.cs
+++ iTraceVS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("iTraceVS Plugin")]
+[assembly: AssemblyTitle("iTrace VS Plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("iTraceVS Plugin")]
+[assembly: AssemblyProduct("iTrace VS Plugin")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- iTraceVS/itrace_window.cs
+++ iTraceVS/itrace_window.cs
@@ -23,7 +23,7 @@
         /// </summary>
         public itrace_window() : base(null)
         {
-            this.Caption = "iTraceVS Plugin";
+            this.Caption = "iTrace VS Plugin";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
